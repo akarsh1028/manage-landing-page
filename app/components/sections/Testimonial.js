39f26acc4a1727cd0b71c672861d70972915bc3c
Akarsh Sharma <akarshsharma001@gmail.com>
2023-12-08 01:37:03 +0530

    scroll addes

--- app/components/sections/Testimonial.js
+++ app/components/sections/Testimonial.js
@@ -5,9 +5,9 @@ import Cards from '../inputs/Cards'
 
 const Testimonial = () => {
   return (
-    <section className='spacing flex flex-col gap-10 items-center py-12 my-28'>
+    <section className='testimonial spacing flex flex-col gap-10 items-center py-12 my-28'>
       <h2 className='text-darkblue text-4xl font-bold mb-16'>What they’ve said</h2>
-      <div className='flex gap-8 '>
+      <div className='flex gap-8 pt-9 overflow-x-auto w-full no-scrollbar'>
         { reviews.map((item) => (
           <Cards key={item.name} item={item}/>
         ) ) }
