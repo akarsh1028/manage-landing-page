fa8c11d6dee685a4d8303b2ea456872f4f73e494
Akarsh Sharma <akarshsharma001@gmail.com>
2023-12-09 00:17:06 +0530

    issue fix

--- app/components/inputs/Cards.js
+++ app/components/inputs/Cards.js
@@ -5,7 +5,7 @@ function Cards({ item, index }) {
   return (
     <div id={`review-${index}`} className='flex-shrink-0 pt-14 max-md:w-full w-[543px]'>
       <div className='relative pt-14 md:p-10 p-6 bg-verylightgray flex justify-center items-center flex-col gap-5 '>
-        <Image priority={false} className='absolute top-[-35px]' src={item.imgURL} alt={item.name} width={70} height={70} />
+        <Image priority className='absolute top-[-35px]' src={item.imgURL} alt={item.name} width={70} height={70} />
         <h2 className='font-bold text-darkblue'>{item.name}</h2>
         <p className='text-darkgrayishblue text-center max-md:text-sm max-md:leading-7'>{item.feedback}</p>
       </div>
